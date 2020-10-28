8e534df90acfd4ac6c705afc4204adfde7049fe1
Jana Warri <j_warri@hotmail.com>
2020-10-27 21:07:00 -0500

    Adding config file and stuff

---
+++ app.py
@@ -0,0 +1,24 @@
+# import necessary libraries
+
+from flask import Flask, render_template
+
+# create instance of Flask app
+
+app = Flask(__name__)
+
+
+# create route that renders index.html template
+@app.route("/")
+def index():
+    # Store the entire team collection in a list
+    animals = list(db.ShelterData.find())
+    print(animals)
+    # IMPORTANT: Flask expects us to adhere to a particular folder structure.
+    # It expects that all html pages will be held in the `templates` directory.
+    # Because of this, we don't need to pass a relative path to the html page. It automatically
+    # assumes that it's in the templates directory.
+    return render_template("index.html", roster=animals)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
