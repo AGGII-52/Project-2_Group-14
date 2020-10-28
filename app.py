011e72b4e2f8fba03eb9d80d25404ec8c9012013
AGGII-52 <66852548+AGGII-52@users.noreply.github.com>
2020-10-27 20:51:26 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ app = Flask(__name__)
 # create route that renders index.html template
 @app.route("/")
 def index():
+    # Store the entire team collection in a list
+    animals = list(db.ShelterData.find())
+    print(animals)
     # IMPORTANT: Flask expects us to adhere to a particular folder structure.
     # It expects that all html pages will be held in the `templates` directory.
     # Because of this, we don't need to pass a relative path to the html page. It automatically
     # assumes that it's in the templates directory.
-    return render_template("index.html", CHRIS_TEST="HELLO WORLD!!")
-
+    return render_template("index.html", roster=animals)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
